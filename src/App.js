882758a18bfd4e9dfe5f723d849175c5ababd36e
Ruslan Khait <rusty6916@gmail.com>
2022-04-05 12:22:55 -0400

    Add Fighter component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import Fighter from "./components/Fighter.js"
 
 function App() {
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
-
+        <Fighter name={'Roy'} color={'red'}/>
+        <Fighter name={'Fox'} color={'green'}/>
+        <Fighter name={'Ganondorf'} color={'purple'}/>
       </div>
     </div>
   );
 }
+
+// const Fighter = {name: 'Roy', color: 'red'}
 
 export default App;
