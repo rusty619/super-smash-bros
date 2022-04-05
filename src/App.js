a942d1ef47e4e1da76fcbe2d6646863dd6f202e1
Ruslan Khait <rusty6916@gmail.com>
2022-04-05 17:30:12 -0400

    add character array of objects with array map method

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Fighter from "./components/Fighter.js"
 import FighterScreen from './components/FighterScreen';
 import { useState } from "react"
 
+const characters = [
+  {name: "Roy",color: "red"},
+  {name: "Fox", color: "green"},
+  {name: "Ganondorf", color: "purple"},
+  {name: "Sheik", color: "lightskyblue"}
+]
+
 function App() {
   const [visible, setVisible] = useState(false)
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
-        <Fighter name={'Roy'} color={'red'} setVisible={setVisible}/>
-        <Fighter name={'Fox'} color={'green'} setVisible={setVisible}/>
-        <Fighter name={'Ganondorf'} color={'purple'} setVisible={setVisible} />
-        <Fighter name={'Sheik'} color={'lightskyblue'} setVisible={setVisible}/>
+        {characters.map((element,i) => {
+          return <Fighter name={element.name} color={element.color} setVisible={setVisible}/>
+        })}
       </div>
       <FighterScreen  isVisible={visible} setVisible={setVisible}/>
     </div>
