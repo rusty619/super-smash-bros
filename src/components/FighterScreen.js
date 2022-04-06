2e6065e43fa1a0dbfdab78cb4539b96858166506
Ruslan Khait <rusty6916@gmail.com>
2022-04-06 11:30:02 -0400

    designed fighter screen

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,12 +1,12 @@
 const FighterScreen = ({ isVisible, setVisible, selectCharacter}) => {
     return(
         <div onClick={() => { setVisible(false) }} className='fighter-screen' style={{ display: isVisible ? "block" : "none"}}>
-            <span className="name-bg">{selectCharacter.name}</span>
-            <h2 className="name-fg">{selectCharacter.name}</h2>
+            <span className="name-bg">{selectCharacter.name.replace('_', ' ')}</span>
+            <h2 className="name-fg">{selectCharacter.name.replace('_', ' ')}</h2>
             <img className="char-portrait" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectCharacter.name.toLowerCase()}/main.png`} alt="" />
-            <p className="fighter-desc">
+            {/*<p className="fighter-desc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur molestias quibusdam culpa provident magni. Numquam similique, deserunt voluptates illo, sunt vel delectus doloremque quae reprehenderit voluptas dolorum harum perspiciatis assumenda.
-            </p>
+            </p>*/}
         </div>
     )
 }
